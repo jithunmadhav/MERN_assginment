414d4ffbc4f3c6aa78aebeda3da8c870d2e9fe74
jithunmadhav <jithunmadhav1@gmail.com>
2023-11-24 02:01:46 +0530

    cors updated

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ dbconnect()
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000'],
+  origin: ['http://localhost:3000','https://mern-nk0y.onrender.com'],
   credentials: true,
 }));
 
