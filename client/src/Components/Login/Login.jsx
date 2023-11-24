558c18e20c309b5a2093f588064470faa936d68a
jithunmadhav <jithunmadhav1@gmail.com>
2023-11-24 12:52:51 +0530

    UI  updated

--- client/src/Components/Login/Login.jsx
+++ client/src/Components/Login/Login.jsx
@@ -19,7 +19,7 @@ function Copyright(props) {
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
       <Link color="inherit" href="https://github.com/jithunmadhav">
-        CONNECT
+        MERN_APP
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
